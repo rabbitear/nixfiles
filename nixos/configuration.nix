66cd760808547ada336e2724869f945c549a0208
rabbitear <weatchu@gmail.com>
2025-09-29 16:42:27 -0800

    Add GNOME settings to NixOS configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -180,33 +180,8 @@
     enableSSHSupport = true;
   };
 
-  programs.dconf.profiles.user.databases = [
-    {
-      lockAll = true;
-      settings = {
-        "org/gnome/desktop/interface" = {
-          accent-color = "green";
-        };
-        "org/gnome/desktop/input-sources" = {
-          xkb-options = [ "ctrl:nocaps" ];
-        };
-        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-	    "org/gnome/mutter".dynamic-workspaces = false;
-        "org/gnome/desktop/wm/preferences".num-workspaces = "4";
-        "org/gnome/desktop/wm/keybindings" = {
-	      switch-to-workspace-1 = [ "<Control>1" "<Super>1" "<F1>" ];
-          switch-to-workspace-2 = [ "<Control>2" "<Super>2" "<F2>" ];
-          switch-to-workspace-3 = [ "<Control>3" "<Super>3" "<F3>" ];
-          switch-to-workspace-4 = [ "<Control>4" "<Super>4" "<F4>" ];
-	      move-to-workspace-1 = [ "<Shift>F1" ];
-          move-to-workspace-2 = [ "<Shift>F2" ];
-          move-to-workspace-3 = [ "<Shift>F3" ];
-          move-to-workspace-4 = [ "<Shift>F4" ];
-	      toggle-fullscreen = [ "<Super>F" ];
-	    };
-      };
-    }
-  ];
+  # For x230, Its Ivybridge CPU does not work well with vulkun
+  # use ngl or opengl with GSK_RENDERER and apps will appear.
   environment.variables = {
      GSK_RENDERER = "ngl";
   };
