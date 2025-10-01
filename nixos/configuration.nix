d948a9b903d34dea0990983404d49d9cc2dc2f20
rabbitear <weatchu@gmail.com>
2025-09-30 19:59:18 -0800

    chore(nixos): Update workspace shortcuts in NixOS config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -106,10 +106,10 @@
           switch-to-application-2 = [ "" ];
           switch-to-application-3 = [ "" ];
           switch-to-application-4 = [ "" ];
-	      switch-to-workspace-1 = [ "<Super>1" ];
-          switch-to-workspace-2 = [ "<Super>2" ];
-          switch-to-workspace-3 = [ "<Super>3" ];
-          switch-to-workspace-4 = [ "<Super>4" ];
+	      switch-to-workspace-1 = [ "F1" ];
+          switch-to-workspace-2 = [ "F2" ];
+          switch-to-workspace-3 = [ "F3" ];
+          switch-to-workspace-4 = [ "F4" ];
 	      move-to-workspace-1 = [ "<Control><Shift>1" ];
           move-to-workspace-2 = [ "<Control><Shift>2" ];
           move-to-workspace-3 = [ "<Control><Shift>3" ];
