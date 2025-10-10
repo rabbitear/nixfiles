40623f2917a0fda191d8fdec86285eb25b0a8bec
rabbitear <weatchu@gmail.com>
2025-10-09 18:31:00 -0800

    Add custom shell application to home-manager configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -63,6 +63,19 @@
   };
 
   # programs.neovim.enable = true;
+  # =====
+  # ktr - got this from tonybtw, oct 9, 2025
+  home.packages = with pkgs; [
+    (pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        nix-search-tv
+      ];
+      text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+    })
+  ];
+
   home.packages = with pkgs; [ 
     rustup
     superTuxKart
